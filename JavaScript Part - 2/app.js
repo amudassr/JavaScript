48f4c31e4087df0aefdf021c9891a71311ad83cf
amudassr <amudassirx@gmail.com>
2026-04-22 18:32:44 +0500

    Operators in JS - Arithmetic, Unary, Assignment

--- JavaScript Part - 2/app.js
+++ JavaScript Part - 2/app.js
@@ -1,13 +1,57 @@
+// console.log() 
 console.log("Hello World!");
 console.log("welcome to my programming journey");
-
 let a = 5;
 let b = 8;
 console.log("the sum is :", a + b);
 
 
+// Template Literals 
 let pencilPrice = 10;
 let erassorPrice = 5;
 // let result = "this is sum : " + (pencil + erassor) +  "Rupees";
 let result = `the total sum is : ${pencilPrice + erassorPrice} Rupees.`;
 console.log(result);
+
+
+// Arthimatic Operators (+, -, *, /, %, **)
+let x = 10;
+let y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+
+// Uniary Operators 
+let m = 10;
+console.log(m++);
+console.log(++m);
+
+
+
+// Assignment Operators 
+let c = 10;
+let d = 5;
+c = d;
+console.log(c);
+
+let age = 21;
+age += 1;
+console.log(age);
+
+age -= 2;
+console.log(age);
+
+age *= 2;
+console.log(age);
+
+age /= 2;
+console.log(age);
+
+age %= 2;
+console.log(age);
+
+
