a869f1106941bb046aa266ded67a26cb3521f2c4
amudassr <amudassirx@gmail.com>
2026-04-22 18:10:20 +0500

    Template Literals

--- JavaScript Part - 2/app.js
+++ JavaScript Part - 2/app.js
@@ -3,4 +3,11 @@ console.log("welcome to my programming journey");
 
 let a = 5;
 let b = 8;
-console.log("the sum is :" , a + b);
+console.log("the sum is :", a + b);
+
+
+let pencilPrice = 10;
+let erassorPrice = 5;
+// let result = "this is sum : " + (pencil + erassor) +  "Rupees";
+let result = `the total sum is : ${pencilPrice + erassorPrice} Rupees.`;
+console.log(result);
