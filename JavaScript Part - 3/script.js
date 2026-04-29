f92238bfd1816861a0d96e06bb0162e94d301854
amudassr <amudassirx@gmail.com>
2026-04-29 21:50:43 +0500

    Method Chaining

--- JavaScript Part - 3/script.js
+++ JavaScript Part - 3/script.js
@@ -1,2 +1,27 @@
 let mssg = "ILoveCoading";
 
+let message = "    hello   ";
+
+// let newMessage = message.trim();
+// console.log("after trim : ", message);
+
+// newMessage = newMessage.toUpperCase();
+// console.log("after uppercase : ", newMessage);
+
+let newMessage = message.trim().toUpperCase();
+console.log(newMessage);
+
+
+let result = "  pass   ";
+// console.log(result);
+
+// let newResult = result.trim();
+// console.log("after trim : ", newResult);
+
+
+// let newResult = result.toUpperCase();
+// console.log("after uppercase : " , newResult);
+
+
+let newResult = result.trim().toUpperCase();
+console.log(newResult);
