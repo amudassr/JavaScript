4b80ab4b4846020c1ee6960a474970f6e84ed662
amudassr <amudassirx@gmail.com>
2026-04-30 22:22:43 +0500

    Slice Method

--- JavaScript Part - 3/script.js
+++ JavaScript Part - 3/script.js
@@ -1,27 +1,54 @@
-let mssg = "ILoveCoading";
+
+// Methods with Arguments - indexOf 
+
+let myMssg = "ILoveCoading";
+let myResult = myMssg.indexOf("v");
+console.log(myResult);
+
+
+// Method Chaining 
 
 let message = "    hello   ";
+console.log(message)
 
-// let newMessage = message.trim();
-// console.log("after trim : ", message);
+let newMessage = message.trim();
+console.log("after trim : ", newMessage);
 
-// newMessage = newMessage.toUpperCase();
-// console.log("after uppercase : ", newMessage);
-
-let newMessage = message.trim().toUpperCase();
-console.log(newMessage);
+newMessageCap = message.toUpperCase();
+console.log("after uppercase : ", newMessageCap);
 
 
-let result = "  pass   ";
-// console.log(result);
-
-// let newResult = result.trim();
-// console.log("after trim : ", newResult);
+// print pass 
+let result = "  pass    ";
+console.log(result);
 
 
-// let newResult = result.toUpperCase();
-// console.log("after uppercase : " , newResult);
-
-
-let newResult = result.trim().toUpperCase();
+// print pass with trim 
+let newResult = result.trim();
 console.log(newResult);
+
+
+// print pass with toUpperCase 
+let newResultCap =result.toUpperCase();
+console.log(newResultCap);
+
+
+// print pass with trim & toUpperCase 
+let finalResult = result.trim().toUpperCase();
+console.log(finalResult);
+
+
+
+// Slice Method 
+
+// 1st Method  (Starting + Ending Index)
+let name = "Ali Hamza";
+console.log(name.slice(0, 4));
+
+// 2nd Method (Only Starting Index)
+let fName = "Gul Hassan";
+console.log(fName.slice(4, fName.length));
+
+// 3rd Method (Negative Value)
+let stuId = "bc2404";
+console.log(stuId.slice(-6)); 
