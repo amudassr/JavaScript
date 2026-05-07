1b3c91184c850ee14de2195836e4e420f411ab44
amudassr <amudassirx@gmail.com>
2026-05-07 08:30:07 +0500

    for Loops in JS

---
+++ JavaScript Part - 4/script.js
@@ -0,0 +1,17 @@
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+
+
+// for loop
+
+//  Basic Counter
+for(let i = 1; i<=10; i++) {
+    console.log(i); 
+};
+
+for(j=10; j>=1; j = j -2){
+    console.log(j)
+};
