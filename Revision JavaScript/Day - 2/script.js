8a2a9e965d9d094b1361bcdfd0310f5136a480da
amudassr <amudassirx@gmail.com>
2026-06-19 07:13:26 +0500

    String Methods: trim method, immutable strings, to Upper & Lower Case, methods with arguments - indexOf

---
+++ Revision JavaScript/Day - 2/script.js
@@ -0,0 +1,42 @@
+// String Methods 
+
+// trim method 
+
+let mssg = "    hello    ";
+console.log(mssg.trim());
+
+
+// let name = prompt("enter your name");
+// console.log(name.trim());
+
+// let pass = prompt("enter your password");
+// let newPass = pass.trim();
+// console.log(newPass);
+
+
+// Immutable strings in js
+
+
+// ToUpperCase and ToLowerCase 
+
+let firstName = "Muhammad";
+let modifyUpper = firstName.toUpperCase();
+let modifyLower = firstName.toLowerCase();
+
+console.log(modifyLower);
+console.log(modifyUpper);
+console.log(firstName);
+
+
+// Methods with Arguments - indexOf 
+
+let str = "ILoveCoading";
+let newStr = str.indexOf("Coading");
+console.log(newStr);
+
+let text = "welcome";
+let newText = text.indexOf("z");
+console.log(newText);
+
+
+
