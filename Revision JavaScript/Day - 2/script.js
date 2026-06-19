86ee2ab2e4fe19ba9de93a3fcc59fcd6df5f6465
amudassr <amudassirx@gmail.com>
2026-06-19 21:56:50 +0500

    Creating Arrays, Mutable Arrays, Array Methods, indexOf & includes methods, concatenation & reverse methods

--- Revision JavaScript/Day - 2/script.js
+++ Revision JavaScript/Day - 2/script.js
@@ -39,4 +39,176 @@ let newText = text.indexOf("z");
 console.log(newText);
 
 
+// Method Chaining 
 
+let message = ("Muhammad");
+let newMessage = message.trim().toUpperCase();
+console.log(newMessage);
+
+
+
+// Slice Method 
+
+let res = "Destinction";
+let newRes = res.slice(0, 3);
+
+// let newRes = res.slice(3);
+// let newRes = res.slice(3, res.length);
+// let newRes = res.slice(3, 11);
+
+// let newRes = res.slice(-10);
+console.log(newRes);
+
+
+
+// Replace & Repeat Method 
+
+let rep = "IloveCoading";
+// let newRep = rep.replace("love", "do");
+let newRep = rep.replace("o", "x");
+console.log(newRep);
+
+
+// Repeat Method 
+
+let fruit = "Mango";
+let newFruit = fruit.repeat(4);
+console.log(newFruit);
+
+
+
+// Practise Qs : 1 
+
+let msg = "help!";
+let newMsg = msg.trim().toUpperCase();
+console.log(newMsg);
+
+
+// Practise Qs : 2 
+
+let collegeName = "ApnaCollege";
+// let collegeRes = collegeName.slice(4,11);
+// let collegeRes = collegeName.indexOf("na");
+let collegeRes = collegeName.replace("Apna", "Our");
+console.log(collegeRes);
+
+
+
+// Practise Qs : 3
+
+collegeRes = collegeName.slice(4, collegeName.length).replace("l", "t").replace("l", "t");
+console.log(collegeRes);
+
+
+// Array 
+
+let students = ["Abdullah", "Haider", "Ali", "Umer"];
+console.log(students);
+console.log(students.length);
+
+let marks = [99, 85, 73, 68, 100];
+console.log(marks);
+
+// Mixed Array 
+let info = ["hasan", 25, 6.1];
+console.log(info);
+console.log(info[0][0]);
+
+// Empty Array 
+let empArr = [];
+console.log(empArr);
+
+
+// Arrays are Mutable 
+
+let fruits = ["mango", "apple", "orange"];
+console.log(fruits);
+
+fruits[0] = "banana";
+console.log(fruits);
+
+fruits[1] = "pinapple";
+console.log(fruits);
+
+fruits[5] = "grapes";
+console.log(fruits);
+console.log(fruits[0][0]);
+
+fruits[8] = "papaya";
+console.log(fruits);
+
+
+// Array Methods 
+// push method 
+
+let cars = ["audi", "bmw", "xuv", "toyota"];
+console.log(cars);
+let newCars = cars.push("suzuki");
+console.log(cars);
+
+// pop method 
+
+newCars = cars.pop();
+console.log(newCars);
+console.log(cars);
+
+// unshift method 
+
+newCars = cars.unshift("mercedes");
+console.log(cars);
+
+// shift method 
+
+newCars = cars.shift();
+console.log(newCars);
+console.log(cars);
+
+
+// Practise Qs : 1
+
+let followers = ["Hamid", "Anas", "Taha"];
+let blocked = followers.shift();
+console.log(followers);
+console.log(blocked);
+
+// Practise Qs : 2
+
+let monthStart = ['january', 'july', 'march', 'august'];
+let monthEnd = monthStart.shift();
+monthEnd = monthStart.shift();
+monthEnd = monthStart.unshift("june");
+monthEnd = monthStart.unshift("july");
+console.log(monthStart);
+
+
+// Array Method - indexOf 
+
+let primary = ["red", "yellow", "blue"];
+let primaryColor = primary.indexOf("blue");
+console.log(primaryColor);
+
+// Array Method - includes 
+
+let secondary = ["black", "white", "gray"];
+let secondaryColor = secondary.includes("white");
+console.log(secondaryColor);
+
+
+// Concatenation  
+
+let coloring = primary.concat(secondary);
+console.log(coloring);
+
+
+// Reverse 
+
+let reverse = primary.reverse();
+console.log(reverse);
+
+console.log(primary);
+
+
+
+// Slice in Arrays 
+
+let 
