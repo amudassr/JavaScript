7aff29751b7f35c8c2a8756a6fef627ce2c979ad
amudassr <amudassirx@gmail.com>
2026-06-29 18:44:55 +0500

    Add, Update & delete  Object key values

--- JavaScript Part - 5/script.js
+++ JavaScript Part - 5/script.js
@@ -30,4 +30,42 @@ let post = {
 };
 
 console.log(post.username);
-console.log(post.tags[1]);
+console.log(post.tags);
+
+
+// Reserved word value get 
+
+const obj = {
+    1: "a",
+    2: "b",
+    true: "c",
+    null: "d",
+    undefined: "e"
+};
+
+console.log(obj[undefined]);
+
+// Add & Update value 
+
+const studensData = {
+    name : "Muhammad",
+    age : 22,
+    marks : 95.4,
+    city : "karachi"
+};  
+
+studensData.city = "lahore";
+console.log(studensData);
+studensData.gender = "male";
+console.log(studensData.gender);
+console.log(studensData);
+studensData.marks = "A";
+console.log(studensData.marks);
+console.log(studensData);
+studensData.marks = [81,88,93];
+console.log(studensData.marks);
+console.log(studensData);
+delete studensData.marks;
+console.log(studensData);
+studensData.marks = [91,89,79];
+console.log(studensData);
