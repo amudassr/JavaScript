96aaa16eec72c62cb427b576477213d5aff3566a
amudassr <amudassirx@gmail.com>
2026-07-01 16:48:57 +0500

    Functions, Function with Arguments, Return

---
+++ JavaScript Part - 5/assign.js
@@ -0,0 +1,29 @@
+// Qs 1 
+
+let dice = Math.floor(Math.random()*6 + 1);
+console.log(dice);
+
+// Qs 2
+
+let car = {
+    name : "AQua",
+    model : "2018",
+    color : ["silver", "gray","white"]
+};
+
+console.log(car.name);
+
+
+// Qs 3 
+
+let person = {
+    name : "Muhammad",
+    age : 22,
+    city : "Karachi"
+}
+
+console.log(person);
+console.log(person.city = "New York");
+console.log(person);
+console.log(person.country = "United States");
+console.log(person);
