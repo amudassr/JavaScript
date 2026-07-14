ff1f52ac02e7f4b28a3b684a25e3cdd6ee69718f
amudassr <amudassirx@gmail.com>
2026-07-14 16:03:47 +0500

    this keyword, try & catch

---
+++ JavaScript Part - 7/script.js
@@ -0,0 +1,34 @@
+// this keyword 
+
+// let students = {
+//     name: "Muhammad",
+//     age: 22,
+//     eng: 91,
+//     math: 93,
+//     sci: 95,
+//     getAvg() {
+//         console.log(this);
+//         let avg = (this.eng + this.math + this.sci) / 3;
+//         console.log(`${this.name} got average marks = ${avg}`);
+//     }
+// }
+
+// students.getAvg();
+
+
+// try & catch 
+
+console.log("hello");
+console.log("hello");
+try {
+    console.log(a);
+} catch(err) {
+    console.log("caught an error... a is not define");
+    console.log(err);
+}
+
+console.log("hello2");
+console.log("hello2");
+console.log("hello2");
+
+
