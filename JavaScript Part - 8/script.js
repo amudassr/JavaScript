caa83ae312a87942bffa84f88f738375e79813e5
amudassr <amudassirx@gmail.com>
2026-07-22 21:58:29 +0500

    Array Methods - forEach

--- JavaScript Part - 8/script.js
+++ JavaScript Part - 8/script.js
@@ -10,7 +10,7 @@ let print = function (el) {
 
 arr.forEach(print);
 
-
+// e.g : 1
 
 let arrayEach = [10, 12, 14, 16, 18];
 
@@ -20,6 +20,9 @@ let printEach = function (each) {
 
 arrayEach.forEach(printEach);
 
+
+// e.g : 2
+
 let singleArr = [99, 101, 41, 87, 27];
 
 let printSingle = function (single) {
@@ -27,3 +30,15 @@ let printSingle = function (single) {
 }
 
 singleArr.forEach(single);
+
+
+// e.g : 3 
+
+let newArr = [29, 12, 23, 24, 44, 19];
+
+let printNewArr = function (arr) {
+    console.log(arr);
+}
+
+newArr.forEach(arr);
+
